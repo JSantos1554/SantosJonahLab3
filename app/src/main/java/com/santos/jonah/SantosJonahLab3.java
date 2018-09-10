f7b6c5b7b2cd2aa77b5eabd46abff991c2c833c7
Jonah <jonahmae.santos.iics@ust.edu.ph>
2018-09-10 19:00:34 +0800

    Initial commit

---
+++ app/src/main/java/com/santos/jonah/SantosJonahLab3.java
@@ -0,0 +1,13 @@
+package com.santos.jonah;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SantosJonahLab3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lab3);
+    }
+}
